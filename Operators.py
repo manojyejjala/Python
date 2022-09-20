9bd462d3843b6fd94d2f87e163ccb12904a9b4f5
manojyejjala <113358637+manojyejjala@users.noreply.github.com>
2022-09-20 23:29:02 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,10 @@
+x=float(input("Enter value of X="))
+y=float(input("Enter value of Y="))
+
+print("Addition of x+y=",x+y)
+print("Substraction of x-y=",x-y)
+print("Multiplication of x*y=",x*y)
+print("Division of x/y=",x/y)
+print("Modulus of x%y=",x%y)
+print("Exponentiation of x**y=",x**y)
+print("Floor division of x//y=",x//y)
